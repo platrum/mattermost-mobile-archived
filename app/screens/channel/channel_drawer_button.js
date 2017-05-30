8ba42b4f43ebcbe8f2c09c2cc1ac9bd55ba40774
enahum <nahumhbl@gmail.com>
2017-05-30 08:56:31 -0400

    Have the hamburger badge follow the theme (#586)

---
+++ app/screens/channel/channel_drawer_button.js
@@ -0,0 +1,196 @@
+// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {
+    PanResponder,
+    Platform,
+    StyleSheet,
+    TouchableOpacity,
+    View
+} from 'react-native';
+import DeviceNotification from 'react-native-push-notification';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import Badge from 'app/components/badge';
+import {getTheme} from 'app/selectors/preferences';
+import {preventDoubleTap} from 'app/utils/tap';
+import {makeStyleSheetFromTheme} from 'app/utils/theme';
+
+import {getUnreadsInCurrentTeam} from 'mattermost-redux/selectors/entities/channels';
+import {getCurrentTeamId, getTeamMemberships} from 'mattermost-redux/selectors/entities/teams';
+import EventEmitter from 'mattermost-redux/utils/event_emitter';
+
+class ChannelDrawerButton extends PureComponent {
+    static propTypes = {
+        applicationInitializing: PropTypes.bool.isRequired,
+        currentTeamId: PropTypes.string.isRequired,
+        myTeamMembers: PropTypes.object,
+        theme: PropTypes.object,
+        messageCount: PropTypes.number,
+        mentionCount: PropTypes.number
+    };
+
+    static defaultProps = {
+        currentChannel: {},
+        theme: {},
+        messageCount: 0,
+        mentionCount: 0
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            opacity: 1
+        };
+    }
+
+    componentWillMount() {
+        this.panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onMoveShouldSetPanResponder: () => true,
+            onStartShouldSetResponderCapture: () => true,
+            onMoveShouldSetResponderCapture: () => true,
+            onResponderMove: () => false
+        });
+    }
+
+    componentDidMount() {
+        EventEmitter.on('drawer_opacity', this.setOpacity);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        DeviceNotification.setApplicationIconBadgeNumber(nextProps.mentionCount);
+    }
+
+    componentWillUnmount() {
+        EventEmitter.off('drawer_opacity', this.setOpacity);
+    }
+
+    setOpacity = (value) => {
+        this.setState({opacity: value > 0 ? 0.1 : 1});
+    };
+
+    handlePress = () => {
+        EventEmitter.emit('open_channel_drawer');
+    };
+
+    render() {
+        const {
+            applicationInitializing,
+            currentTeamId,
+            mentionCount,
+            messageCount,
+            myTeamMembers,
+            theme
+        } = this.props;
+
+        if (applicationInitializing) {
+            return null;
+        }
+
+        let mentions = mentionCount;
+        let messages = messageCount;
+
+        const members = Object.values(myTeamMembers).filter((m) => m.team_id !== currentTeamId);
+        members.forEach((m) => {
+            mentions = mentions + m.mention_count;
+            messages = messages + m.msg_count;
+        });
+
+        let badge;
+        let badgeCount = mentions;
+
+        if (!badgeCount && messages) {
+            badgeCount = -1;
+        }
+
+        const style = getStyleFromTheme(theme);
+
+        if (badgeCount !== 0) {
+            badge = (
+                <Badge
+                    style={style.badge}
+                    countStyle={style.mention}
+                    count={badgeCount}
+                    minHeight={5}
+                    minWidth={5}
+                    onPress={() => preventDoubleTap(this.handlePress, this)}
+                />
+            );
+        }
+
+        return (
+            <TouchableOpacity
+                {...this.panResponder.panHandlers}
+                onPress={this.handlePress}
+                style={style.container}
+            >
+                <View style={[style.wrapper, {opacity: this.state.opacity, zIndex: 30}]}>
+                    <Icon
+                        name='md-menu'
+                        size={25}
+                        color={theme.sidebarHeaderTextColor}
+                    />
+                    {badge}
+                </View>
+            </TouchableOpacity>
+        );
+    }
+}
+
+const getStyleFromTheme = makeStyleSheetFromTheme((theme) => {
+    return StyleSheet.create({
+        container: {
+            width: 40
+        },
+        wrapper: {
+            alignItems: 'center',
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            paddingHorizontal: 10,
+            zIndex: 30
+        },
+        badge: {
+            backgroundColor: theme.mentionBj,
+            borderColor: theme.sidebarHeaderBg,
+            borderRadius: 10,
+            borderWidth: 1,
+            flexDirection: 'row',
+            height: 20,
+            left: 5,
+            padding: 3,
+            position: 'absolute',
+            right: 0,
+            ...Platform.select({
+                android: {
+                    top: 10
+                },
+                ios: {
+                    top: 5
+                }
+            }),
+            width: 20
+        },
+        mention: {
+            color: theme.mentionColor,
+            fontSize: 10
+        }
+    });
+});
+
+function mapStateToProps(state) {
+    return {
+        applicationInitializing: state.views.root.appInitializing,
+        currentTeamId: getCurrentTeamId(state),
+        myTeamMembers: getTeamMemberships(state),
+        theme: getTheme(state),
+        ...getUnreadsInCurrentTeam(state)
+    };
+}
+
+export default connect(mapStateToProps)(ChannelDrawerButton);
